e247644b67b7b3cae622a4be6bff77415698b78e
Prithvi Raj Singh <prithvirajsingh.dev@gmail.com>
2026-08-06 15:07:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        for (int i=0; i<arr.size(); i++){
-            if(arr[i] > arr[i+1]){
-                return i;
+        
+
+        int st =0;
+        int end= arr.size()-1;
+
+        while (st<=end){
+            int mid=st+(end-st)/2;
+
+
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                return mid;
+            }
+
+            else if(arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1]){
+                end=mid-1;
+            }
+
+            else {
+                st= mid+1;
             }
         }
+
         return 0;
     }
 };
